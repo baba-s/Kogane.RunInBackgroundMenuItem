e78c3d90089bfe3a0646625408c75c76d408e128
baba-s <shotababa1206@gmail.com>
2022-08-27 15:05:13 +0900

    add

---
+++ Editor/RunInBackgroundMenuItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal static class RunInBackgroundMenuItem
+    {
+        private const string MENU_ITEM_NAME = "Kogane/Run In Background";
+
+        static RunInBackgroundMenuItem()
+        {
+            EditorApplication.delayCall            += () => UpdateChecked();
+            EditorApplication.playModeStateChanged += _ => UpdateChecked();
+        }
+
+        [MenuItem( MENU_ITEM_NAME )]
+        private static void Change()
+        {
+            PlayerSettings.runInBackground = !PlayerSettings.runInBackground;
+
+            foreach ( var editorWindow in Resources.FindObjectsOfTypeAll<EditorWindow>() )
+            {
+                editorWindow.Repaint();
+            }
+
+            UpdateChecked();
+        }
+
+        private static void UpdateChecked()
+        {
+            Menu.SetChecked( MENU_ITEM_NAME, PlayerSettings.runInBackground );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.RunInBackgroundMenuItem
+# Kogane Run In Background Menu Item
+
+Run In Background を変更する MenuItem を追加するエディタ拡張
